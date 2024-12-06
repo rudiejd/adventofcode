978fdc94b8501a6197fced5b3f8798026d797066
JD <jd@rudiejd.io>
2024-12-06 17:16:56 -0500

    all i want for christmas is to get good at elixir

--- 2024/elixir/day4/lib/day4.ex
+++ 2024/elixir/day4/lib/day4.ex
@@ -39,12 +39,45 @@ defmodule Day4 do
  Take a look at the little Elf's word search. How many times does XMAS appear?
 """
 
-# reduce the grid, try moving in every direction, check if the match exists
-@spec word_search(list(list(String.t()))) :: non_neg_integer()
-def word_search(grid) do
+@dirs [{0, 1}, {1, 0}, {1, 1}, {0, -1}, {-1, 0}, {-1, -1}, {1, -1}, {-1, 1}]
 
+# TODO turn this into a map
+@spec at(list(list(String.t())), Position.t()) :: non_neg_integer()
+def at(grid, {x, y}) do
+  if length(grid) <= y or length(grid) <= x do
+    nil
+  else 
+    Enum.at(grid, y) |> Enum.at(x)
+  end
+end
+
+def go({x, y}, {x1, y1}) do
+  {x + x1, y + y1}
+end
+
+defp do_word_search(grid, pos, []), do: 1 |> dbg()
+defp do_word_search(grid, pos, [first_char|rest]) do
+  case at(grid, pos) do
+     ^first_char -> 
+      Enum.reduce(@dirs, 0, fn d, acc -> 
+        acc + do_word_search(grid, go(pos, d), rest) end)
+     _ -> 0
+  end
   
 end
 
+@doc """
+
+  Check the number of times "XMAS" occurs in a word search
+"""
+# reduce the grid, try moving in every direction, check if the match exists
+@spec word_search(list(list(String.t()))) :: non_neg_integer()
+def word_search(grid) do
+  Enum.reduce(0..length(grid), 0, fn y, acc ->
+    acc + Enum.reduce(0..length(grid), 0, fn x, a -> 
+      a + do_word_search(grid, {x, y}, ~c'XMAS')
+    end)
+  end)
+end
 
 end
--- 2024/elixir/day4/test/day4_test.exs
+++ 2024/elixir/day4/test/day4_test.exs
@@ -2,7 +2,14 @@ defmodule Day4Test do
   use ExUnit.Case
   doctest Day4
 
-  test "greets the world" do
-    assert Day4.hello() == :world
+  test "do the problem" do
+    contents = File.read!('./test/day4input')
+    rows = contents
+    |> String.split("\n")
+    |> Enum.map(&String.to_charlist/1)
+    |> Enum.reject(&length(&1) < 1)
+
+    IO.inspect(Day4.word_search(rows))
   end
+
 end
