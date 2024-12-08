c5666961b2ea3b2a9bd97498581e9a6f063713d5
JD Rudie <jd@rudiejd.io>
2024-12-08 15:42:20 -0500

    day 5 brute force lol

--- 2024/elixir/day5/lib/day5.ex
+++ 2024/elixir/day5/lib/day5.ex
@@ -3,16 +3,51 @@ defmodule Day5 do
   Documentation for `Day5`.
   """
 
-  @doc """
-  Hello world.
+  def apply_ordering(rules, updates) do
+    updates
+    |> Enum.reduce(0, fn update, acc -> 
 
-  ## Examples
+      out_of_order = Enum.with_index(update)
+      |> Enum.any?(fn {val, idx} -> 
+        Enum.slice(update, 0, idx)
+        |> Enum.any?(&rules[val] && MapSet.member?(rules[val], &1))
+        end)
+      case out_of_order do
+        true -> acc
+        false -> acc + Enum.at(update, div(length(update), 2))
+      end
+    end)
+  end
 
-      iex> Day5.hello()
-      :world
 
-  """
-  def hello do
-    :world
+  def read_input() do
+    [raw_rules, raw_updates] = File.read!('./test/day5input')
+    |> String.split("\n\n", parts: 2)
+
+    rules = raw_rules
+    |> String.split("\n")
+    |> Enum.reduce(%{}, fn val, map -> 
+      parts = val
+      |> String.split("|")
+      [left, right] = parts
+
+      left_num = String.to_integer(left)
+      right_num = String.to_integer(right)
+      case Map.get(map, left_num) do
+        %MapSet{} = set -> Map.put(map, left_num, MapSet.put(set, right_num))
+        nil -> Map.put(map, left_num, MapSet.new([right_num]))
+      end
+    end)
+    |> Map.new
+
+    updates = raw_updates
+    |> String.split("\n")
+    |> Enum.reject(&String.length(&1) <= 0)
+    |> Enum.map(fn rules -> 
+      String.split(rules, ",") 
+      |> Enum.map(&String.to_integer/1)
+    end)
+
+    {rules, updates}
   end
 end
--- 2024/elixir/day5/test/day5_test.exs
+++ 2024/elixir/day5/test/day5_test.exs
@@ -2,7 +2,18 @@ defmodule Day5Test do
   use ExUnit.Case
   doctest Day5
 
-  test "greets the world" do
-    assert Day5.hello() == :world
+  test "do the problem" do
+    [raw_rules, raw_updates] = File.read!('./test/day5input')
+    |> String.split("\n\n", parts: 2)
+
+    rules = raw_rules
+    |> String.split("\n")
+    |> Enum.flat_map(fn val -> 
+      parts = val
+      |> String.split("|")
+      [left, right] = parts
+      [{String.to_integer(left), String.to_integer(right)}]
+    end)
+
   end
 end
