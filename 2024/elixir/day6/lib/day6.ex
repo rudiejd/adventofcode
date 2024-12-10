c8893d72e13f4cd4e7c335f13dca257d088e6dcd
JD Rudie <jd@rudiejd.io>
2024-12-10 08:11:35 -0500

    day 6 progress

--- 2024/elixir/day6/lib/day6.ex
+++ 2024/elixir/day6/lib/day6.ex
@@ -3,16 +3,47 @@ defmodule Day6 do
   Documentation for `Day6`.
   """
 
-  @doc """
-  Hello world.
 
-  ## Examples
+  defp go({x, y}, {x1, y1}) do
+    {x + x1, y + y1}
+  end
 
-      iex> Day6.hello()
-      :world
+  @dir_idx %{{0, -1} => 0, {1, 0} => 1, {0, 1} => 2, {-1, 0} => 3}
+  @dirs %{0 => {0, -1}, 1 => {1, 0}, 2 => {0, 1}, 3 => {-1, 0}}
+  defp guard_wander(grid, guard_pos, dir \\ {0, -1}, cnt \\ 0) do
+    next_pos = go(guard_pos, dir)
+    dbg()
+    case grid[next_pos] do 
+      ?# -> guard_wander(grid, guard_pos, @dirs[rem(@dir_idx[dir] + 1, length(Map.keys(@dirs)))], cnt)
+      ?.-> guard_wander(grid, go(guard_pos, dir), dir, cnt + 1)
+      ?^-> guard_wander(grid, go(guard_pos, dir), dir, cnt + 1)
+      _ -> cnt
+    end
+  end
 
-  """
-  def hello do
-    :world
+  def part1(grid) do
+     {guard_pos, _val}= grid
+      |> Enum.filter(fn {{_, _}, val} -> val == ?^ end)
+      |> hd
+
+      guard_wander(grid, guard_pos)
+  end
+
+
+  def read_input() do
+    contents = File.read!('./test/day6input')
+
+    # I stole this from the other problem!
+    map = contents
+    |> String.split("\n")
+    |> Enum.with_index
+    |> Enum.flat_map(fn {line, row} ->
+      String.to_charlist(line)
+      |> Enum.with_index
+      |> Enum.map(fn {char, col} ->
+        {{col, row}, char}
+      end)
+    end)
+    |> Map.new
   end
 end
--- 2024/elixir/day6/test/day6_test.exs
+++ 2024/elixir/day6/test/day6_test.exs
@@ -2,7 +2,22 @@ defmodule Day6Test do
   use ExUnit.Case
   doctest Day6
 
-  test "greets the world" do
-    assert Day6.hello() == :world
+  test "do the problem" do
+
+    contents = File.read!('./test/day6input')
+
+    # I stole this from the other problem!
+    map = contents
+    |> String.split("\n")
+    |> Enum.with_index
+    |> Enum.flat_map(fn {line, row} ->
+      String.to_charlist(line)
+      |> Enum.with_index
+      |> Enum.flat_map(fn {char, col} ->
+        position = {row, col}
+        [{position, char}]
+      end)
+    end)
+    |> Map.new
   end
 end
